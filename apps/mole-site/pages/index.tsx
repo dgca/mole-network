0aab6338619c4123e95bd86b0bc420895abc20ec
Daniel Cortes <danielgcortes@gmail.com>
2023-03-04 22:54:22 -0700

    Update website content

--- apps/mole-site/pages/index.tsx
+++ apps/mole-site/pages/index.tsx
@@ -7,6 +7,9 @@ import {
   VStack,
   useBreakpointValue,
   Stack,
+  Spacer,
+  UnorderedList,
+  ListItem,
 } from '@chakra-ui/react';
 import Image from 'next/image';
 import { NavBar } from '../components/NavBar/NavBar';
@@ -96,95 +99,214 @@ export function Index() {
         >
           <Stack gap={12}>
             <Stack gap={2}>
-              <Heading color={h2Color} mb={4}>
-                Problem statement
-              </Heading>
-              <Text fontSize="lg">
-                When building a decentralized protocol, we often require data
-                that isn&apos;t available on-chain. There are a number of Oracle
-                solutions that provide access to web2 API data, or meatspace
-                data such as the weather; but one type of data that is
-                underserved by current tools is data that can be derived from
-                things that happen on-chain.
-              </Text>
-              <Text fontSize="lg">
-                Though we like to say that on-chain data can be freely used by
-                any contract, this is not always true. For example, event data
-                is inaccessible from contracts, reading private contract values
-                can be difficult, and processing data from multiple contracts to
-                get to the value we need can get very expensive if done through
-                a smart contract transaction.
-              </Text>
-            </Stack>
-            <Stack gap={2}>
-              <Heading color={h2Color} mb={4}>
-                Solution
-              </Heading>
-              <Text fontSize="lg">
-                We&apos;re building Mole Network, a system that lets smart
-                contract developers define the data get the data they need from
-                on-chain events and transactions.
-              </Text>
-              <Text fontSize="lg">
-                In a nutshell, Mole Network brings pairs people who want data,
-                and people who want to report data for a small fee.
-              </Text>
-              <Text fontSize="lg">The system has three primary parts:</Text>
-            </Stack>
-            <Stack gap={2}>
+              <Heading color={h2Color}>What it is, exactly</Heading>
+              <Spacer />
+
               <Heading as="h3" size="lg" color={h3Color}>
-                Users
+                Consumers
               </Heading>
+              <Text fontSize="lg">
+                Mole Network lets users who need data from on or more on and
+                off-chain sources get that data, transform it in whatever way
+                they want, and send that transformed data to a destination which
+                can be on-chain and (soon to be) off-chain. Let&apos;s call
+                these user types Consumers.
+              </Text>
+              <Text fontSize="lg">
+                Consumers create an ETL (Extract, Transform, Load) Spec which
+                describes what data sources to pull data from, what functions to
+                run that data through, and which destinations the output should
+                be sent to. Data sources and destinations will include an
+                expanding list of items, as the system is flexible enough for
+                new items to be added easily.
+              </Text>
+              <Text fontSize="lg">
+                At the current time, the sources can be events emitted on any
+                EVM chain, as well as any web2 API endpoint.
+              </Text>
+              <Text fontSize="lg">
+                Currently, destinations can be any contract on any EVM chain. In
+                the future, that can expand to anything that can receive a
+                network request, including web2 endpoints, and non-EVM chains.
+              </Text>
+
+              <Spacer />
+
+              <Heading as="h3" size="lg" color={h3Color}>
+                Data Reporters
+              </Heading>
+              <Text fontSize="lg">
+                Data Reporters are the ones who fulfill Consumers&apos;
+                requests. Consumers run a program that monitors the data
+                sources, and when they receive relevant data, they run the data
+                through the Consumer&apos;s ETL spec, and write the result to
+                its destination.
+              </Text>
+
+              <Spacer />
+
+              <Heading as="h3" size="lg" color={h3Color}>
+                Incentives
+              </Heading>
+              <Text fontSize="lg">
+                Data Reporters do not do this work for free, especially since
+                writing data on-chain costs gas. In order to incentivize Data
+                Reporters to report their data, Consumers will fund their ETL
+                Specs with tips that the Data Reporters will receive for
+                fulfilling their data requests.
+              </Text>
+              <Text fontSize="lg">
+                Conversely, Data Reporters will be incentivized to publish
+                correct data by needing to stake some amount of tokens in order
+                to be allowed to report. If another reporter thinks someone is
+                being duplicitous, they can report them; triggering a dispute
+                mechanism where the party at fault will lose their stake and the
+                counter party will be rewarded for keeping the system in check.
+              </Text>
+            </Stack>
+
+            <Stack gap={2}>
+              <Heading color={h2Color}>The Vision</Heading>
+              <Spacer />
+
+              <Heading as="h3" size="lg" color={h3Color}>
+                Oracle? Bridge? Keeper?
+              </Heading>
+              <Text fontSize="lg">
+                Mole Network is neither, all, and more. We&apos;re intentionally
+                thinking about Mole Network as a flexible communication layer
+                between arbitrary sources and destinations. As the project
+                grows, we&apos;re excited to see what other use cases our
+                community comes up with.
+              </Text>
+
+              <Spacer />
+
+              <Heading as="h3" size="lg" color={h3Color}>
+                Governance and ownership
+              </Heading>
+              <Text fontSize="lg">
+                We fully believe in the power of decentralization, and we intend
+                Mole Network to be owned by our users. Both Data Reporters and
+                Consumers will be granted ownership based on how much they use
+                the system, which will allow them to decide the future of the
+                protocol.
+              </Text>
+            </Stack>
+
+            <Stack gap={2}>
+              <Heading color={h2Color}>Roadmap</Heading>
+              <Spacer />
+
+              <Heading as="h3" size="lg" color={h3Color}>
+                Alpha
+              </Heading>
+              <Text fontSize="lg">
+                We are here. Much of the functionality this project needs is in
+                progress, but we hope the idea is clear and the vision is
+                present. Currently, things we know must be done are:
+              </Text>
+
+              <UnorderedList px={8}>
+                <ListItem mb={4}>
+                  <Text fontSize="lg">
+                    Assessing additional ways to reduce data manipulation such
+                    as requiring multiple reporters to submit a value before the
+                    data is written.
+                  </Text>
+                </ListItem>
+                <ListItem mb={4}>
+                  <Text fontSize="lg">
+                    Creating the token mechanism by which Data Reporters become
+                    eligible to report.
+                  </Text>
+                </ListItem>
+                <ListItem mb={4}>
+                  <Text fontSize="lg">
+                    Assessing the current ETL Spec format for any risks that it
+                    could pose to data reporters.
+                  </Text>
+                </ListItem>
+                <ListItem mb={4}>
+                  <Text fontSize="lg">
+                    Creating a repository of ETL Specs so that Data Reporters
+                    can find jobs for them to run.
+                  </Text>
+                </ListItem>
+              </UnorderedList>
+
+              <Spacer />
+
+              <Heading as="h3" size="lg" color={h3Color}>
+                Beta
+              </Heading>
+              <Text fontSize="lg">
+                During beta, we&apos;ll be taking user feedback and
+                incorporating it into the platform. We&apos;ll also be setting
+                up the governance structure that will eventually make Mole
+                Network a community-led project.
+              </Text>
+
+              <Spacer />
+
+              <Heading as="h3" size="lg" color={h3Color}>
+                Launch
+              </Heading>
+              <Text fontSize="lg">
+                Launch is when we&apos;re community-run. What that means will be
+                up those who make the governance body. For instance, this could
+                mean turning off upgradability for and having Mole Network be a
+                fully decentralized application. However this manifests,
+                community ownership is the eventual goal for the network.
+              </Text>
+            </Stack>
+
+            <Stack gap={2}>
+              <Heading color={h2Color}>The Technical TL;DR:</Heading>
+              <Spacer />
 
               <Text fontSize="lg">
-                Users are the people who need some arbitrary data made available
-                on-chain. Users specify the data sources they need, how they
-                want the data processed, and where they want the data reported
+                At this time, there are three technical components that make the
+                system work:
               </Text>
-            </Stack>
-            <Stack gap={2}>
-              <Heading as="h3" size="lg" color={h3Color}>
-                Reporters
-              </Heading>
 
-              <Text fontSize="lg">
-                Reporters are the people who want to offer their proecssing
-                power and collect fees in return. Reporters run the Mole Network
-                CLI which tells them what data users have asked for, and it
-                listens to an EVM RPC node to capture those events and write the
-                results to a destination.
-              </Text>
-            </Stack>
-            <Stack gap={2}>
-              <Heading as="h3" size="lg" color={h3Color}>
-                Destinations
-              </Heading>
+              <Spacer />
 
-              <Text fontSize="lg">
-                Destinations are where the results are written. This could be
-                existing decentralized oracle protocols, or a specific smart
-                contract that the user has specified.
-              </Text>
-            </Stack>
-            <Stack gap={2}>
-              <Heading color={h2Color} mb={4}>
-                Future State
+              <Heading as="h3" size="lg" color={h3Color}>
+                The Mole Network CLI
               </Heading>
               <Text fontSize="lg">
-                The above describes a solution for a common problem smart
-                contract developers face today, but the Mole Network system is
-                flexible enough to expand to other use cases as well.
+                The CLI is a tool that Data Reporters run. It consumes ETL
+                Specs, which starts a process where the data sources are
+                monitored, and when data source events happen, the appropriate
+                transformation function is run, and data is optionally returned
+                which, if-present, will be written to a destination.
               </Text>
+
+              <Spacer />
+
+              <Heading as="h3" size="lg" color={h3Color}>
+                ETL Specs
+              </Heading>
               <Text fontSize="lg">
-                For instance, by defining how a user could ask for the result of
-                a web2 API endpoint, we can combine on-chain and off-chain data,
-                process it, and make it available back on-chain.
+                ETL Specs are a piece of code which defines the sources,
+                transformations, and destinations that a Consumer would like to
+                be run so that they can get the data they need. Because they are
+                fully written in code, there is no ambiguity about what the
+                Consumer wants to happen.
               </Text>
+
+              <Spacer />
+
+              <Heading as="h3" size="lg" color={h3Color}>
+                Scribe contracts
+              </Heading>
               <Text fontSize="lg">
-                Also, since Mole Network works on any EVM chain, it can act as a
-                data bridge between blockchains. I.e. user can ask that events
-                be captured from multiple chains, processed, and have the data
-                written back to one single chain.
+                For data whose destination is on-chain, the data is sent to a
+                Scribe contract. The Scribe receives the data, as well as the
+                contract address it should be headed to, and the function which
+                should receive the data. It&apos;s up to the Consumer to ensure
+                that function handles receiving the data in the appropriate way.
               </Text>
             </Stack>
           </Stack>
